e2c7d77a6bab70c52fb4d6de47438e6394e1aa4a
dhggh574 <fjgihgjvc68@gmail.com>
2024-05-20 12:23:37 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,10 @@
+document.getElementById('accountForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var email = document.getElementById('email').value;
+
+  // Create account logic here
+
+});
